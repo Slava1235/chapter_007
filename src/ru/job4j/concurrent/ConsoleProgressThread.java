469eeb8aa594bf037b939c5ee14f21a4b1f34bfb
Slava1235 <starslava14@gmail.com>
2021-01-06 22:01:24 +0500

    3.  Interrupt thread

---
+++ src/ru/job4j/concurrent/ConsoleProgressThread.java
@@ -0,0 +1,33 @@
+package ru.job4j.concurrent;
+
+class ConsoleProgress implements Runnable {
+
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
+            try {
+                Thread.sleep(500);
+                System.out.print("\r load: " +  " \\|/");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+public class ConsoleProgressThread {
+
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Thread progress = new Thread(new ConsoleProgress());
+        progress.start();
+
+        Thread.sleep(1); /* симулируем выполнение параллельной задачи в течение 1 секунды. */
+        progress.interrupt(); //
+
+    }
+}
+
+
+
