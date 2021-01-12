e642ac341b4dfbef1ef2bcecfb4e3a169ef50df5
Slava1235 <starslava14@gmail.com>
2021-01-12 16:52:11 +0500

    2. JCIP

---
+++ src/ru/job4j/CountTest.java
@@ -0,0 +1,36 @@
+package ru.job4j;
+
+import org.testng.annotations.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CountTest {
+    private class ThreadCount extends Thread {
+        private final Count count;
+
+        private ThreadCount(final Count count) {
+            this.count = count;
+        }
+
+        @Override
+        public void run() {
+            this.count.increment();
+        }
+    }
+    @Test
+    public void whenExecute2ThreadThen2() throws InterruptedException {
+        final Count count = new Count();
+
+        Thread first = new ThreadCount(count);
+        Thread second = new ThreadCount(count);
+
+        first.start();
+        second.start();
+
+        first.join();
+        second.join();
+
+        assertThat(count.get(), is(2));
+    }
+}
