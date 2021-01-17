77dbbe0b744702b609bf51058ef6814f6824a588
Slava1235 <starslava14@gmail.com>
2021-01-18 01:39:46 +0500

    4. ThreadSafe динамический список

---
+++ src/ru/job4j/synch/SingleLockList.java
@@ -0,0 +1,33 @@
+package ru.job4j.synch;
+
+
+import ru.job4j.DynamicContainer;
+
+import java.util.Iterator;
+
+public class SingleLockList<T> implements Iterable<T> {
+    DynamicContainer<T> dynamicContainer;
+
+    public synchronized void add(T value) {
+        dynamicContainer.add(value);
+    }
+
+    public synchronized T get(int index) {
+      return dynamicContainer.get(index);
+    }
+
+    @Override
+    public synchronized Iterator<T> iterator() {
+      return copy(this.dynamicContainer).iterator();
+    }
+
+    private Iterable<T> copy(DynamicContainer<T> dynamicContainer) {
+        DynamicContainer<T> list = new DynamicContainer<>();
+        if(dynamicContainer != null) {
+
+            list.iterator().forEachRemaining(dynamicContainer::add);
+        }
+            return list;
+
+    }
+}
