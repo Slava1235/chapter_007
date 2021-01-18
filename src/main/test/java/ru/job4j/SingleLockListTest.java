2173dfea4c3235af2432eb8b59b9f2e0a289f373
Slava1235 <starslava14@gmail.com>
2021-01-18 21:58:15 +0500

    added test

---
+++ src/main/test/java/ru/job4j/SingleLockListTest.java
@@ -0,0 +1,27 @@
+package ru.job4j;
+
+import org.junit.Test;
+import ru.job4j.synch.SingleLockList;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class SingleLockListTest {
+
+    @Test
+    public void add() throws InterruptedException {
+        SingleLockList<Integer> list = new SingleLockList<>();
+        Thread first = new Thread(() -> list.add(1));
+        Thread second = new Thread(() -> list.add(2));
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        Set<Integer> rsl = new TreeSet<>();
+        list.iterator().forEachRemaining(rsl::add);
+        assertThat(rsl, is(Set.of(1, 2)));
+    }
+}
