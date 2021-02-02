513b4d2dd3a6c5ed0a4b08d0a08901c2c27e4cf4
Slava1235 <starslava14@gmail.com>
2021-02-02 16:21:29 +0500

    2. Обеспечить остановку потребителя. [#442231]

---
+++ src/main/java/ru/job4j/buffer/ParallelSearch.java
@@ -0,0 +1,39 @@
+package ru.job4j.buffer;
+
+import ru.job4j.producerconsumer.SimpleBlockingQueue;
+
+public class ParallelSearch {
+
+    public static void main(String[] args) {
+        SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<Integer>();
+        final Thread consumer = new Thread(
+                () -> {
+                    while (!Thread.currentThread().isInterrupted()) {
+                        try {
+                            System.out.println(queue.poll());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+
+        new Thread(
+                () -> {
+                    for (int index = 0; index != 3; index++) {
+                        try {
+                            queue.offer(index);
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        consumer.interrupt();
+                    }
+                }
+        ).start();
+
+    }
+}
+
