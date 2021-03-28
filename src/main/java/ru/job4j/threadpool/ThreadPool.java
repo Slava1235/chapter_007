67fc95e781d21b669964dbf3c626fa1904423503
Slava1235 <starslava14@gmail.com>
2021-03-28 15:07:23 +0500

    1. Реализовать ThreadPool [#442229]

--- src/main/java/ru/job4j/threadpool/ThreadPool.java
+++ src/main/java/ru/job4j/threadpool/ThreadPool.java
@@ -1,40 +1,48 @@
 package ru.job4j.pool;
 
 import ru.job4j.producerconsumer.SimpleBlockingQueue;
-
 import java.util.LinkedList;
 import java.util.List;
 
+
 public class ThreadPool {
-    private final List<Thread> threads = new LinkedList<>();
-    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>();
-    private final int size = Runtime.getRuntime().availableProcessors();
+    private static final int POOL_SIZE = Runtime.getRuntime().availableProcessors();
+    private final List<Thread> pool = new LinkedList<>();
+    private final SimpleBlockingQueue<Runnable> tasks = new SimpleBlockingQueue<>(10);
+
+    public ThreadPool() {
+        for (int i = 0; i < POOL_SIZE; i++) {
+            pool.add(new Thread(
+                    () -> {
+                        try {
+                            while (!Thread.currentThread().isInterrupted()) {
+                                tasks.poll().run();
+                                System.out.println(Thread.currentThread().getName());
+                            }
+                        } catch (Exception e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+            ));
+        }
+        pool.forEach(Thread::start);
+    }
 
 
     public void work(Runnable job) {
-        for (int i = 0; i < size; i++) {
-            threads.add(new Thread(() -> {
-            }));
-            tasks.offer(job);
-        }
-
-
-        for (Thread thread : threads) {
-            thread.start();
-
-        }
-
+        tasks.offer(job);
     }
-
 
     public void shutdown() {
-        for (Thread t : threads) {
-            t.interrupt();
-        }
+        pool.forEach(Thread::interrupt);
     }
 
-    public static void main(String[] args) {
-        new ThreadPool().work(new Thread());
-        new ThreadPool().shutdown();
-    }
+//    public static void main(String[] args) {
+//      ThreadPool threadPool = new ThreadPool();
+//        for (int i = 0; i < 4; i++) {
+//            int count = i;
+//            threadPool.work(() -> System.out.println("Task " + count));
+//        }
+//        threadPool.shutdown();
+//    }
 }
